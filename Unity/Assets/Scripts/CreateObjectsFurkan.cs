356cbdfbd330b374ac3e5819f09b3f974d73997c
afeser <afeser@outlook.com.tr>
2017-02-11 21:34:38 +0300

    Assets moved
    Some code modifications.
    Assets moved.

---
+++ Unity/Assets/Scripts/CreateObjectsFurkan.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CreateObjectsFurkan : MonoBehaviour {
+
+    // *** instantiate function parameters are going to be revised 
+
+    //GameObject Prefabs
+    public Transform obstacle1;
+    public Transform obstacle2;
+    public Transform obstacleLast;
+    public Transform food1;
+    public Transform food2; //assuming there will be 2 food objects, can always be increased
+    public Transform airObject;
+
+
+    private int numberOfVariousObstacles; // baglama, cell phone, elektrik supurgesi, screw, vosvos = 5 , can always be increased
+    private int consequentObstaclesSpawned = 0, consequentFoodsSpawned = 0, consequentAirSpawned = 0;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    private void ObjectSpawner()
+    {
+        int randomObject = Random.Range(0, 2);
+        if (consequentObstaclesSpawned == 0)        // the last object was a boost
+        {
+            SpawnRandomObstacle();
+        }
+        else if (consequentObstaclesSpawned == 2)   // the last two objects were obstacles
+        {
+            SpawnRandomBoost();
+        }
+        else                                        // the last object was an obstacle
+        {
+            switch(randomObject)
+            {
+                case 0:
+                    SpawnRandomBoost();
+                    break;
+                case 1:
+                    SpawnRandomObstacle();
+                    break;
+            }
+        }
+    }
+
+    private void SpawnRandomObstacle()
+    {
+        int randomObstacle = Random.Range(0, numberOfVariousObstacles);
+        switch (randomObstacle)
+        {
+            case 0:
+                Instantiate(obstacle1);
+                break;
+                    // ...
+                    // ...
+            case 4:                         // numberOfVariousObstacles = 5
+                Instantiate(obstacleLast);
+                break;
+        }
+        consequentObstaclesSpawned++;
+        consequentFoodsSpawned = 0; consequentAirSpawned = 0;
+    }
+
+    private void SpawnRandomBoost()
+    {
+        int randomBoost = Random.Range(0, 2); //food or air
+        
+        // boost will be either food or air
+        // after 2 consequent foods, air will spawn
+        // after 2 consequent air objects, food will spawn
+        if (consequentFoodsSpawned == 2)
+        {
+            SpawnAirObject();
+        }
+        else if (consequentAirSpawned == 2)
+        {
+            SpawnFood();
+        }
+        else
+        {
+            switch (randomBoost)
+            {
+                case 0:
+                    SpawnAirObject();
+                    break;
+                case 1:
+                    SpawnFood();
+                    break;
+            }
+        }
+        consequentObstaclesSpawned = 0;
+    }
+    
+    void SpawnAirObject()
+    {
+        Instantiate(airObject);
+        consequentAirSpawned++;
+        consequentFoodsSpawned = 0;
+    }
+         
+    void SpawnFood()
+    {
+        int randomFood = Random.Range(0, 2); //food1 or food2
+        switch (randomFood)
+        {
+            case 0:
+                Instantiate(food1);
+                break;
+            case 1:
+                Instantiate(food2);
+                break;
+        }
+        consequentFoodsSpawned++;
+        consequentAirSpawned = 0;
+	}
+
+    }
---
+++ Unity/Assets/Scripts/ParachuteDynamics.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParachuteDynamics : MonoBehaviour {
+
+    /*
+     * THIS SCRIPT CONTAINS THE BEHAVIOURS OF THE PARACHUTE AND BACKGROUND
+     */
+
+    public GameObject player;
+    public GameObject background;
+
+    /*
+     * THIS VARIABLES DEPEND UPON SCREEN RESULOTION AND SHOULD BE MODIFIED
+     * WHEN SETTING THIS PROJECT FOR MULTI DEVICES
+     */
+    private Vector3 playerInitialPosition = new Vector3(0, 3.12F, 0);
+    private float fallSpeedperFrame = 0.03f;
+    private float backgroundOffset = 0; // To prevent background from disappearing
+
+
+	// Use this for initialization
+	void Start () {
+        //Setting initial parameters
+        player.transform.position = playerInitialPosition;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (background.transform.position.y > backgroundOffset) { //Move player
+            player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y - fallSpeedperFrame, player.transform.position.z); //Move downward
+        }else { //Move background
+            if(Camera.main.pixelHeight)
+            background.transform.position = new Vector3(background.transform.position.x, background.transform.position.y + fallSpeedperFrame, background.transform.position.z); //Move upward
+        }
+
+
+	}
+
+}
